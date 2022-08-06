cdcec89430d109e43046de996582ced345b26a7f
Mehrad Kavian <mehrad.kavian@gmail.com>
2022-08-06 16:37:23 -0400

    added token struct

--- pallets/royalty/src/lib.rs
+++ pallets/royalty/src/lib.rs
@@ -6,16 +6,33 @@
 /// https://eips.ethereum.org/EIPS/eip-2981
 use frame_support::pallet_prelude::*;
 
+use sp_std::vec::Vec;
+
 pub use pallet::*;
+
+
 
 #[frame_support::pallet]
 pub mod pallet {
+	use super::*;
 
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
 	pub struct Pallet<T>(_);
 
+	#[derive(Encode, Decode, Clone, RuntimeDebug, PartialEq, Eq)]
+	pub struct Token<T: Config> {
+		pub id: u32,
+		pub owner: T::AccountId,
+		pub royalty_precentage: u16,
+		pub royalty_receiver: T::AccountId,
+		pub uri: Vec<u8>, // for usage of text
+	}
+
+
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
+
 	}
+
 }
