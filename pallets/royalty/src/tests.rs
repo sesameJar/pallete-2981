db703396f4463112873571c9f7ef87d8d33b4b0a
Mehrad Kavian <mehrad.kavian@gmail.com>
2022-08-09 17:32:13 -0400

    more tests for mint fn

--- pallets/royalty/src/tests.rs
+++ pallets/royalty/src/tests.rs
@@ -1,14 +1,25 @@
-use crate::{mock::*, Error};
+use crate::{mock::*, Token};
 use frame_support::{assert_noop, assert_ok};
 
 #[test]
 fn can_mint() {
 	new_test_ext().execute_with(|| {
 		let token_uri = "uri1".as_bytes().to_vec();
-		// Dispatch a signed extrinsic.
-		assert_ok!(RoyaltyModule::mint(Origin::signed(100), token_uri, 200, 1000));
-		// Read pallet storage and assert an expected result.
-		// assert_eq!(TemplateModule::something(), Some(42));
+
+		assert_ok!(RoyaltyModule::mint(Origin::signed(100), token_uri.clone(), 1000, 200));
+		assert_eq!(RoyaltyModule::token_pointer(), 1);
+
+		let token = Token{
+			id: 0,
+			owner: 100,
+			royalty_receiver: 200,
+			royalty_percentage: 1000,
+			uri: token_uri.clone()
+		};
+
+		assert_eq!(RoyaltyModule::tokens(0, &100), Some(token.clone()));
+
+		// System::assert_last_event(Event::RoyaltyModule(crate::Event::<Test>::TokenMinted(100, 0, token_uri)));
 	});
 }
 
